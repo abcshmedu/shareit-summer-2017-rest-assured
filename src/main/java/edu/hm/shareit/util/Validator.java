b09f3ebc746f267774a3f776d355096ea89b6588
Andrea Limmer <andrea-limmer@gmx.de>
2017-05-04 10:45:22 +0200

    isbn validator

--- src/main/java/edu/hm/shareit/util/Validator.java
+++ src/main/java/edu/hm/shareit/util/Validator.java
@@ -8,12 +8,56 @@ package edu.hm.shareit.util;
 
 /**
  * @author Wolfgang Gabler, wgabler@hm.edu
+ * @author Andrea Limmer, limmer@hm.edu
  * @since 03.05.17
  */
 public abstract class Validator {
 
+	/**
+	 * Validates an ISBN-13.
+	 * @param isbn ISBN to validate (ISBN-13)
+	 * @return if ISBN is valid
+	 * 
+	 * source: http://www.moreofless.co.uk/validate-isbn-13-java/
+	 */
     public static boolean isValidIsbn(String isbn) {
-        return false;
+    	if ( isbn == null )
+        {
+            return false;
+        }
+
+        //remove any hyphens
+        isbn = isbn.replaceAll( "-", "" );
+
+        //must be a 13 digit ISBN
+        if ( isbn.length() != 13 )
+        {
+            return false;
+        }
+
+        try
+        {
+            int tot = 0;
+            for ( int i = 0; i < 12; i++ )
+            {
+                int digit = Integer.parseInt( isbn.substring( i, i + 1 ) );
+                tot += (i % 2 == 0) ? digit * 1 : digit * 3;
+            }
+
+            //checksum must be 0-9. If calculated as 10 then = 0
+            int checksum = 10 - (tot % 10);
+            if ( checksum == 10 )
+            {
+                checksum = 0;
+            }
+
+            return checksum == Integer.parseInt( isbn.substring( 12 ) );
+        }
+        catch ( NumberFormatException nfe )
+        {
+            //to catch invalid ISBNs that have non-numeric characters in them
+            return false;
+        }
     }
 
     public static boolean isValidBarcode(String barcode) {
