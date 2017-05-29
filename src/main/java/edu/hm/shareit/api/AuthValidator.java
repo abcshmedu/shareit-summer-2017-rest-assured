d2963a7ce3ef5940eee1b09d5de8ca937d9975bb
wolfgang <wgabler@hm.edu>
2017-05-29 19:30:06 +0200

    Checkstyle AuthValidator

--- src/main/java/edu/hm/shareit/api/AuthValidator.java
+++ src/main/java/edu/hm/shareit/api/AuthValidator.java
@@ -9,21 +9,41 @@ package edu.hm.shareit.api;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
+import javax.ws.rs.core.Response;
+
 /**
+ * Check validity of jwt token with auth service.
+ *
  * @author Wolfgang Gabler, wgabler@hm.edu
  * @since 29.05.17
  */
-public class AuthValidator {
+public final class AuthValidator {
 
+    /**
+     * URL for checking jet validity with auth service.
+     */
     private static final String URL_AUTH_VALIDATE = "https://shareit-auth-rest-assured.herokuapp.com/shareit/auth/validate";
 
+    /**
+     * Check validity of jwt token with auth service.
+     *
+     * @param jwt jwt token to check.
+     * @return true if token is valid, false otherwise.
+     */
     public static boolean checkValidity(String jwt) {
         try {
             final int status = Unirest.get(URL_AUTH_VALIDATE + "/" + jwt).asBinary().getStatus();
-            return status == 200;
+            return status == Response.Status.OK.getStatusCode();
         } catch (UnirestException e) {
             return false;
         }
+    }
+
+    /**
+     * Hidden ctor.
+     */
+    private AuthValidator() {
+
     }
 
 }
