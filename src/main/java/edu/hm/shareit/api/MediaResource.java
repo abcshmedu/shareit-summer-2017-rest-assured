b61ccd9f792c7d31071577e4dccab536dc42b0ee
wolfgang <wgabler@hm.edu>
2017-05-29 19:32:25 +0200

    Checkstyle MediaResource

--- src/main/java/edu/hm/shareit/api/MediaResource.java
+++ src/main/java/edu/hm/shareit/api/MediaResource.java
@@ -52,6 +52,7 @@ public class MediaResource {
      * Add a book.
      *
      * @param book Book to add.
+     * @param jwt  The User's jwt token.
      * @return Response.
      */
     @POST
@@ -60,7 +61,7 @@ public class MediaResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response addBook(Book book, @PathParam("jwt") String jwt) {
         if (!AuthValidator.checkValidity(jwt)) {
-            return Response.status(401).build();
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         }
         MediaServiceResult result = mediaService.addBook(book);
         return Response.status(result.getCode()).entity(result.getStatus()).build();
@@ -70,6 +71,7 @@ public class MediaResource {
      * Get a book.
      *
      * @param isbn ISBN of the book.
+     * @param jwt  The User's jwt token.
      * @return Response with the book if available.
      */
     @GET
@@ -77,7 +79,7 @@ public class MediaResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBook(@PathParam("isbn") String isbn, @PathParam("jwt") String jwt) {
         if (!AuthValidator.checkValidity(jwt)) {
-            return Response.status(401).build();
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         }
         Medium book = mediaService.getBook(isbn);
         ObjectMapper mapper = new ObjectMapper();
@@ -93,6 +95,7 @@ public class MediaResource {
     /**
      * Get all books.
      *
+     * @param jwt The User's jwt token.
      * @return Response with all books.
      */
     @GET
@@ -100,7 +103,7 @@ public class MediaResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getBooks(@PathParam("jwt") String jwt) {
         if (!AuthValidator.checkValidity(jwt)) {
-            return Response.status(401).build();
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         }
         return getMedia(mediaService.getBooks());
     }
@@ -110,6 +113,7 @@ public class MediaResource {
      *
      * @param isbn ISBN of the book.
      * @param book Book to update.
+     * @param jwt  The User's jwt token.
      * @return Response.
      */
     @PUT
@@ -118,7 +122,7 @@ public class MediaResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response updateBook(@PathParam("isbn") String isbn, Book book, @PathParam("jwt") String jwt) {
         if (!AuthValidator.checkValidity(jwt)) {
-            return Response.status(401).build();
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         }
         MediaServiceResult result;
         if (isbn.equals(book.getIsbn())) {
@@ -133,6 +137,7 @@ public class MediaResource {
      * Add a disc.
      *
      * @param disc Disc.
+     * @param jwt  The User's jwt token.
      * @return Response.
      */
     @POST
@@ -141,7 +146,7 @@ public class MediaResource {
     @Consumes(MediaType.APPLICATION_JSON)
     public Response addDisc(Disc disc, @PathParam("jwt") String jwt) {
         if (!AuthValidator.checkValidity(jwt)) {
-            return Response.status(401).build();
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         }
         MediaServiceResult result = mediaService.addDisc(disc);
         return Response.status(result.getCode()).entity(result.getStatus()).build();
@@ -151,6 +156,7 @@ public class MediaResource {
      * Get a disc.
      *
      * @param barcode Barcode to identify the disc.
+     * @param jwt     The User's jwt token.
      * @return Response with the disc if available.
      */
     @GET
@@ -158,7 +164,7 @@ public class MediaResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getDisc(@PathParam("barcode") String barcode, @PathParam("jwt") String jwt) {
         if (!AuthValidator.checkValidity(jwt)) {
-            return Response.status(401).build();
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         }
         Medium disc = mediaService.getDisc(barcode);
         ObjectMapper mapper = new ObjectMapper();
@@ -174,6 +180,7 @@ public class MediaResource {
     /**
      * Get all discs.
      *
+     * @param jwt The User's jwt token.
      * @return Response with all discs.
      */
     @GET
@@ -181,7 +188,7 @@ public class MediaResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getDiscs(@PathParam("jwt") String jwt) {
         if (!AuthValidator.checkValidity(jwt)) {
-            return Response.status(401).build();
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         }
         return getMedia(mediaService.getDiscs());
     }
@@ -191,6 +198,7 @@ public class MediaResource {
      *
      * @param barcode Barcode of the disc.
      * @param disc    Disc to be updated.
+     * @param jwt     The User's jwt token.
      * @return Response.
      */
     @PUT
@@ -199,7 +207,7 @@ public class MediaResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response updateDisc(@PathParam("barcode") String barcode, Disc disc, @PathParam("jwt") String jwt) {
         if (!AuthValidator.checkValidity(jwt)) {
-            return Response.status(401).build();
+            return Response.status(Response.Status.UNAUTHORIZED).build();
         }
         MediaServiceResult result;
         if (barcode.equals(disc.getBarcode())) {
