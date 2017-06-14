556ded5f3f8aa7fc2d02c84428936aae4f4d2844
wolfgang <wgabler@hm.edu>
2017-06-14 14:07:16 +0200

    Log4j and deps

---
+++ src/main/java/edu/hm/shareit/ShareItServletContextListener.java
@@ -0,0 +1,37 @@
+package edu.hm.shareit;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
+import edu.hm.shareit.service.MediaService;
+import edu.hm.shareit.service.MediaServiceImpl;
+
+/**
+ * Context Listener to enable usage of google guice together with jersey.
+ */
+public class ShareItServletContextListener extends GuiceServletContextListener {
+
+    private static final Injector injector = Guice.createInjector(new ServletModule() {
+
+        @Override
+        protected void configureServlets() {
+            bind(MediaService.class).to(MediaServiceImpl.class);
+        }
+    });
+
+    /**
+     * This method is only required for the HK2-Guice-Bridge in the
+     * Application class.
+     *
+     * @return Injector instance.
+     */
+    static Injector getInjectorInstance() {
+        return injector;
+    }
+
+    @Override
+    protected Injector getInjector() {
+        return injector;
+    }
+}
